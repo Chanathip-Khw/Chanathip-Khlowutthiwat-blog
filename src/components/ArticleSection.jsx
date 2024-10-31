2080dcf80b07efeef042fe99f09505c9ed3deb8e
Oat Chana <chanathip0944729842@gmail.com>
2024-10-31 15:27:16 +0700

    feat:add category selecter both mobile and desktop

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,6 +1,8 @@
 import searchLight from "../assets/searchLight.svg";
 import expandDownArrow from "../assets/expandDownArrow.svg";
+import checkMark from "../assets/checkMark.svg";
 import { blogPosts } from "@/data/blogPosts";
+import { useState } from "react";
 
 function BlogCard({ blogPost }) {
   return (
@@ -28,27 +30,39 @@ function BlogCard({ blogPost }) {
 }
 
 function ArticleSection() {
+  const categories = ["Highlight", "Cat", "Inspiration", "General"];
+  const [categoryInput, setCategoryInput] = useState("Highlight");
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleCategoryClick = (category) => {
+    setCategoryInput(category);
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="ArticleSection bg-[#F9F8F6] font-[Poppins]  flex flex-col gap-5 md:px-28">
       <h1 className="lastest-article-text  font-[600] text-[24px] p-4 ">
         Latest articles
       </h1>
+
       <div className="sort-section bg-[#EFEEEB] text-[#75716B] md:flex justify-between items-center md:rounded-2xl">
-        <div className="categoty-button hidden md:block">
-          <button className="text-[#75716B] focus:text-[#43403B] focus:bg-[#DAD6D1] p-3 m-4 rounded-lg">
-            Highlight
-          </button>
-          <button className="text-[#75716B] focus:text-[#43403B] focus:bg-[#DAD6D1] p-3 m-4 rounded-lg">
-            Cat
-          </button>
-          <button className="text-[#75716B] focus:text-[#43403B] focus:bg-[#DAD6D1] p-3 m-4 rounded-lg">
-            Inspiration
-          </button>
-          <button className="text-[#75716B] focus:text-[#43403B] focus:bg-[#DAD6D1] p-3 m-4 rounded-lg">
-            General
-          </button>
+        <div className="categoty-button-desktop hidden md:block">
+          {categories.map((category, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => handleCategoryClick(category)}
+                className="text-[#75716B] focus:text-[#43403B] focus:bg-[#DAD6D1] hover:bg-[#F9F8F6] p-3 m-4 rounded-lg"
+              >
+                {category}
+              </button>
+            );
+          })}
         </div>
-        <div className="search-box h-14 flex justify-between items-center bg-white p-3 m-4 rounded-lg">
+
+        <div className="search-box h-14 flex justify-between items-center border-gray-200 bg-white p-3 m-4 rounded-lg">
           <input
             type="text"
             placeholder="Search"
@@ -56,12 +70,32 @@ function ArticleSection() {
           ></input>
           <img src={searchLight} alt="search light icon"></img>
         </div>
-        <div className="category-box md:hidden">
+
+        <div className="category-box-mobile md:hidden mb-4">
           <h2 className="px-4">Category</h2>
-          <div className="expand-down-box h-14 flex justify-between items-center bg-white p-3 mx-4 my-1 rounded-lg">
-            <p>Hightlight</p>
-            <img src={expandDownArrow} alt="expand down arrow icon"></img>
+          <div className="expand-down-box  h-14 flex justify-between items-center border-gray-200 bg-white p-3 mx-4 my-1 rounded-lg">
+            <p>{categoryInput}</p>
+            <button onClick={() => toggleDropdown()}>
+              <img src={expandDownArrow} alt="expand down arrow icon"></img>
+            </button>
           </div>
+          {isOpen && (
+            <ul className="absolute left-1 right-1 bg-white border border-gray-200 rounded-lg shadow-lg mt-2  mx-3">
+              {categories.map((category, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => handleCategoryClick(category)}
+                    className="flex px-4 py-2 text-left hover:bg-gray-100 w-full text-black"
+                  >
+                    {category === categoryInput && (
+                      <img src={checkMark} alt="check-mark"></img>
+                    )}
+                    <span>{category}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
       <div className="blog-cards px-4 grid grid-cols-1 md:grid-cols-2 gap-10 md:mx-[-15px]">
